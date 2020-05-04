96fd05b0972f45946770bc3ac95b94d57dbeade1
Benjamin Land <benland100@gmail.com>
2020-05-04 12:21:36 -0400

    Add SD image creation script / update petalinux build

--- linux/make_sd_image.sh
+++ linux/make_sd_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-guestfish --verbose -N ../rootfs.img=disk:2G <<_EOF_
+guestfish -N ../rootfs.img=disk:2G <<_EOF_
 part-init /dev/sda msdos 
 part-add /dev/sda p 64 204863 
 part-add /dev/sda p 204864 -1
@@ -12,4 +12,4 @@ upload images/linux/rootfs.ext4 /dev/sda2
 e2fsck /dev/sda2 correct:true
 resize2fs /dev/sda2
 _EOF_
-echo 'type=06' | sfdisk ../rootfs.img 0 #partition type is set wrong for some reason
+echo 'type=06' | sfdisk --quiet ../rootfs.img 0 #partition type is set wrong for some reason
