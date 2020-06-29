c7bfff013fa0d3d990e78f69ac4ccf23663e843f
Benjamin Land <benland100@gmail.com>
2020-06-29 16:12:42 -0400

    Implement read/write register skeleton.

--- sw/src/wib_server.cxx
+++ sw/src/wib_server.cxx
@@ -23,7 +23,27 @@ int main(int argc, char **argv) {
         
         std::string reply_str;        
                 
-        if (command.cmd().Is<wib::GetStatus>()) {
+        if (command.cmd().Is<wib::ReadReg>()) {
+            wib::ReadReg read;
+            command.cmd().UnpackTo(&read);
+            printf("read %i bytes at %x + %x\n",read.size(),read.base_addr(),read.offset());
+            wib::RegValue value;   
+            value.set_size(read.size());
+            value.set_offset(read.offset());
+            value.set_base_addr(read.base_addr());       
+            value.set_value(0x0);
+            value.SerializeToString(&reply_str);
+        } else  if (command.cmd().Is<wib::WriteReg>()) {
+            wib::WriteReg write;
+            command.cmd().UnpackTo(&write);
+            printf("write %i bytes at %x + %x = %x\n",write.size(),write.base_addr(),write.offset(),write.value());
+            wib::RegValue value;   
+            value.set_size(write.size());
+            value.set_offset(write.offset());
+            value.set_base_addr(write.base_addr());       
+            value.set_value(write.value());
+            value.SerializeToString(&reply_str);
+        } else if (command.cmd().Is<wib::GetStatus>()) {
             wib::Status status;            
             /** fill status **/
             status.SerializeToString(&reply_str);
