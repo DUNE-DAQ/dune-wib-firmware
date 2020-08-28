840a26a773dce1497f1b1296700e0c00630fca25
Benjamin Land <benland100@gmail.com>
2020-08-28 16:39:47 -0400

    Petalinux (finally) builds reliably again.

--- linux-new/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ linux-new/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -1,19 +1,2 @@
 #include <configs/xilinx_zynqmp.h>
-//#include <configs/platform-auto.h>
-
-#define CONFIG_USB 
-#define CONFIG_DM_USB
-#define CONFIG_DM_USB_GADGET
-
-#define CONFIG_USB_STORAGE
-#define CONFIG_USB_GADGET
-#define CONFIG_USB_GADGET_MANUFACTURER "U-Boot"
-#define CONFIG_USB_GADGET_VENDOR_NUM 0x0
-#define CONFIG_USB_GADGET_PRODUCT_NUM 0x0
-#define CONFIG_USB_GADGET_VBUS_DRAW 2
-#define CONFIG_USB_GADGET_DOWNLOAD
-
-#define CONFIG_USB_DWC3
-#define CONFIG_USB_DWC3_GADGET
-
-#define CONFIG_USB_GADGET_DUALSPEED
+#include <configs/platform-auto.h>
