c5e15bfac156142b829fd6cd06949f2251f269b6
Benjamin Land <benland100@gmail.com>
2020-09-10 09:54:34 -0400

    Revert changes to 2019.1 distribution and use its FSBL
    The FSBL from 2020.1 does not boot. Pending diagnosis.
    For now, use the 2019.1 FSBL!

--- linux-2020.1/make_bootloader.sh
+++ linux-2020.1/make_bootloader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mkimage -c none -A arm -T script -d boot.cmd images/linux/boot.scr
-petalinux-package --boot --u-boot --force --fpga ../*.bit
+petalinux-package --fsbl ../linux-2019.1/images/linux/zynqmp_fsbl.elf --boot --u-boot --force --fpga ../*.bit
