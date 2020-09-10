dc44dfd133fe159bc7874d5475f3b6565edfb073
Benjamin Land <benland100@gmail.com>
2020-09-10 17:00:04 -0400

    Add 2019.1 fsbl to repo to avoid building all that as well.
    Update make_bootloader.sh to use this fsbl.

--- linux-2020.1/make_bootloader.sh
+++ linux-2020.1/make_bootloader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mkimage -c none -A arm -T script -d boot.cmd images/linux/boot.scr
-petalinux-package --fsbl ../linux-2019.1/images/linux/zynqmp_fsbl.elf --boot --u-boot --force --fpga ../*.bit
+petalinux-package --fsbl pre-built/linux/images/zynq_fsbl_plnx2019.elf --boot --u-boot --force --fpga ../*.bit
