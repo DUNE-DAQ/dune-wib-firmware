52f33ac07ae50a12f1b576947a0a222cac01e414
Benjamin Land <benland100@gmail.com>
2020-10-16 18:41:26 -0400

    Add xvcserver to linux distro
    Xilinx Virtual Cabel, JTAG over TCP/IP

--- sw/extras/wib_init.sh
+++ sw/extras/wib_init.sh
@@ -2,3 +2,6 @@
 
 echo "Starting wib_server"
 /bin/wib_server 2>/var/log/wib_server.err >/var/log/wib_server.log &
+
+echo "Starting xvcserver"
+/bin/xvcserver  2>/var/log/xvcserver.err >/var/log/xvcserver.log &
