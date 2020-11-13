1f5a5f46c8521f69ea78459a429fcc2f3d6ef276
Benjamin Land <benland100@gmail.com>
2020-11-12 20:15:36 -0500

    Cleaner i2c transactions for ltc2990 and ltc2991

--- sw/src/sensors.cc
+++ sw/src/sensors.cc
@@ -34,35 +34,30 @@ double read_ad7414_temp(i2c_t *i2c, uint8_t slave) {
 }
 
 void enable_ltc2990(i2c_t *i2c, uint8_t slave) {
-   uint8_t enable_cmd[2] = { 0x01,0x1F };
-   i2c_write(i2c,slave,enable_cmd,2);
-   uint8_t measure_cmd[2] = { 0x02,0xFF };
-   i2c_write(i2c,slave,measure_cmd,2);
+   i2c_reg_write(i2c,slave,0x01,0x1F);
+   i2c_reg_write(i2c,slave,0x02,0xFF);
    usleep(10000);
 }
 
 //ch 1 to ch 4, 5 == Vcc
 uint16_t read_ltc2990_value(i2c_t *i2c, uint8_t slave, uint8_t ch) {
-    uint8_t read_cmd[1] = { (uint8_t)(6+(ch-1)*2) };
-    i2c_write(i2c,slave,read_cmd,1);
+    uint8_t reg = (uint8_t)(6+(ch-1)*2);
     uint16_t value;
-    i2c_read(i2c,slave,(uint8_t*)&value,2);
+    i2c_block_read(i2c,slave,reg,(uint8_t*)&value,2);
     return value&0x3FFF;
     
 }
 
 void enable_ltc2991(i2c_t *i2c, uint8_t slave) {
-   uint8_t en_meas_cmd[2] = { 0x01,0xF8 };
-   i2c_write(i2c,slave,en_meas_cmd,2);
+   i2c_reg_write(i2c,slave,0x01,0xF8);
    usleep(10000);
 }
 
 //ch 1 to ch 8, 9 == T_internal, 10 == Vcc
 uint16_t read_ltc2991_value(i2c_t *i2c, uint8_t slave, uint8_t ch) {
-    uint8_t read_cmd[1] = { (uint8_t)(0xA+(ch-1)*2) };
-    i2c_write(i2c,slave,read_cmd,1);
+    uint8_t reg = (uint8_t)(0xA+(ch-1)*2);
     uint16_t value;
-    i2c_read(i2c,slave,(uint8_t*)&value,2);
+    i2c_block_read(i2c,slave,reg,(uint8_t*)&value,2);
     return value&0x3FFF;
     
 }
