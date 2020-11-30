55aa1d00176f1a95c268667d3e4a3bac7dc13e83
Benjamin Land <benland100@gmail.com>
2020-11-30 09:22:13 -0500

    Use .bit packaged in hardware description

--- linux-2020.1/make_bootloader.sh
+++ linux-2020.1/make_bootloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-petalinux-package --fsbl pre-built/linux/images/zynq_fsbl_plnx2019.elf --boot --u-boot --force --fpga ../*.bit
+petalinux-package --fsbl pre-built/linux/images/zynq_fsbl_plnx2019.elf --boot --u-boot --force --fpga images/linux/*.bit
 
 cd images/linux/
 tar czf bootfs.tar.gz BOOT.BIN boot.scr image.ub 
