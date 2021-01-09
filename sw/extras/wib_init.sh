af236df0ef590dda0236b9dd7707ea790c784b68
Benjamin Land <benland100@gmail.com>
2021-01-09 13:17:11 -0500

    Disable xvcserver startup

--- sw/extras/wib_init.sh
+++ sw/extras/wib_init.sh
@@ -3,5 +3,6 @@
 echo "Starting wib_server"
 /bin/wib_server 2>/var/log/wib_server.err >/var/log/wib_server.log &
 
-echo "Starting xvcserver"
-/usr/bin/xvcserver  2>/var/log/xvcserver.err >/var/log/xvcserver.log &
+#Evidentally this crashes the whole system somehow...
+#echo "Starting xvcserver"
+#/usr/bin/xvcserver  2>/var/log/xvcserver.err >/var/log/xvcserver.log &
